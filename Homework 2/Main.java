c2ef323ef1b562287248156fff246d651e07e739
Jonpsims <jpsims1@asu.edu>
2025-02-12 21:56:32 -0700

    Update Main.java

--- Homework 2/Main.java
+++ Homework 2/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Questions questions = new Questions();
         Answers answers = new Answers();
 
-        questions.addQuestion("What is Java?");
-        answers.addAnswer(1, "Java is a programming language.");
+        questions.addQuestion("What is a Test case?");
+        answers.addAnswer(1, "A test case tests features with a specific input.");
 
         System.out.println("Question: " + questions.getQuestion(1).getText());
         System.out.println("Answer: " + answers.getAnswer(1).getText());
